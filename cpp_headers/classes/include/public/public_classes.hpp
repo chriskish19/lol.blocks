664b999497d89b47529f09f6d0e3d0b615389f22
chriskish19 <68289629+chriskish19@users.noreply.github.com>
2024-11-21 01:30:22 -0600

    setting up includes

---
+++ cpp_headers/classes/include/public/public_classes.hpp
@@ -0,0 +1,7 @@
+#ifndef PUBLIC_CLASSES_LOL_BLOCKS_HEADER_HPP
+#define PUBLIC_CLASSES_LOL_BLOCKS_HEADER_HPP
+
+// private header include
+#include "../private/lol.blocks.classes.include.hpp"
+
+#endif
